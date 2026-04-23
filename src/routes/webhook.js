9a088a3d79ff4170698a6e3373729c94c0078bdd
Mariano <marianoemgonzalez1@gmail.com>
2026-04-23 01:05:43 -0300

    fix: elimino archivos duplicados de la raiz

--- src/routes/webhook.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const webhookController = require('../controllers/webhookController');
-
-router.post('/', webhookController.handleMessage);
-
-module.exports = router;
